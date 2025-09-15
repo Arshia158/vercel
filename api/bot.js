68b93ce61a9c703d6bf72cd9ef4a8070b3c00b43
Arshia158 <145995978+Arshia158@users.noreply.github.com>
2025-09-15 15:38:43 +0330

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -2,12 +2,10 @@ import { Bot } from "grammy";
 
 const bot = new Bot("8326231289:AAH433g4yAVQPM986exCe2ajpgUGjAtq_34");
 
-// وقتی استارت می‌کنه برای کاربر پیام سلام ارسال کن
 bot.command("start", ctx => ctx.reply("سلام"));
 
 export default async function handler(req, res) {
   try {
-    // grammy کد مربوط به پردازش وبهوک تلگرام
     await bot.handleUpdate(req.body);
     res.status(200).send("ok");
   } catch (error) {
